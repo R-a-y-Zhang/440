214854e807d48c1e37a503210a80f3ff8e17d47d
Rui Zhang <r.zhang194@gmail.com>
2017-02-01 23:36:05 -0500

    stuff

--- pathfinder/pathfinder/pf_modules/solver/solver.py
+++ pathfinder/pathfinder/pf_modules/solver/solver.py
@@ -9,12 +9,54 @@ def calculate_manhattan(array, target): # calculating manhattan distance
             min_arr[i][j] = abs(th - i) + abs(tw - j)
     return man_arr
 
-def A(array, c1, c2):
+# vx is the value of a blocked square, any square with that value or which exceeds it is
+# considered inaccessible
+def check_if_valid(array, s, b0, b1, vx):
+    h = s[0]
+    w = s[1]
+    if array[h][w] >= vx: # if square is "blocked"
+        return False
+    if h < b0[0] or h >= b1[0] or w < b0[1] or w >= b1[1]: # if square is out of bounds
+        return False
+    return True
+
+def sum_lists(t1, t2):
+    if len(t1) is not len(t2):
+        return None
+    v = []
+    for i in range(len(t1)):
+        v.append(t1[i] + t2[i])
+    return tuple(v)
+
+def A(array, c1, c2, vx):
+    # initial setup
     sh = c1[0]
     sw = c1[1]
     th = c2[0]
     tw = c2[1]
-    h = calculate_manhattan(array, c2)
-    closed_set = []
-    open_set = [c1]
+    height = len(array)
+    width = len(array[0])
+    h = calculate_manhattan(array, c2) # the heuristic
+    neighbors = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,1), (1,-1), (-1,-1)]
+    # calculation start
+    openset = [(0, c1)] # list of nodes yet to be examined
+    closed = [] # nodes already examined
+    paths = []
+    while len(nodes) is not 0:
+        current = openset.remove[min(openset)] # pulls smallest node
+        cpos = current[1] # coordinates of current
+        ccost = current[0] # current travel cost from start
+        neighbors = map(lambda n: sum_lists(cpos, n), # list of valid neighbors
+                     filter(lambda n: check_if_lambda(array, sum_lists(n, cpos), # finds all valid neighbors
+                                                (0,0), (height,width), vx), neighbors))
+        nclosed = filter(lambda n: n in closed, neighbors) # closed neighbors
+        if c2 in neighbors: # found path
+            return True
+        # getting heuristics
+        hv = [h[n[0]][n[1]] for n in neighbors]
+        # getting travel cost from cpos
+        gv = [ccost + array[n[0]][n[1]] + (4 if ((n[0] != cpos[0]) and (n[1] != cpos[1])) else 0) \
+                for n in neighbors]
+
+        openset += list(zip([hv+gv], neighbors)) # adding neighbors to list
 
---
+++ pathfinder/pathfinder/src/pathfinder/solver.clj
@@ -0,0 +1,10 @@
+(ns pathfinder.solver.solver
+    (:gen-class))
+
+(defn solve
+    [array sw sh fw fh] ;; array, start width, start height, end width, end height
+    )
+
+(defn calculate-manhattan
+    [array fw fh]
+    )
